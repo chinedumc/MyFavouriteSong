a459f843b6610f8671725ec18527515db857d1c8
YourFavCoder <chinedumc@users.noreply.github.com>
2018-12-15 17:02:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 15 15:03:15 2018
+
+@author: 'NEDUM
+"""
+
+#HomeAssignment1
+#A program that prints all the Attributes of my favourite song: Bad by Michael Jackson
+
+#Assigning the song attributes to variables
+
+#The title of my favourite song is: 
+SongTitle = "Bad"
+
+#This favourite song was sang by:
+Artist = "Michael Jackson"
+
+#And release year is:
+YearReleased = "1987"
+
+#The Song Bad was released under a self-titled album
+AlbumTitle = "Bad"
+
+#The album and song have been classed as pop, funk, R&B and more. I chose Pop for simplicity 
+Genre = "Pop"
+
+#How much time you would spend to rocking this song:
+DurationInSeconds = 246 
+
+#The USA Sales certification for an Album with over 1 million sales is Platinum 
+SalesCertification = "Platinum"
+
+#The record label the album was released under
+RecordLabel = "Epic"
+
+#Michael Jackson co-produced the album with Quincy Jones
+Producer = "Quincy Jones"
+CoProducer = "Michael Jackson"
+SongWriter = "Michael Jackson"
+
+"""
+The Print statements for each assignment statement. We use a print function
+ for each assignment statement
+""" 
+print(SongTitle)
+print(Artist)
+print(YearReleased)
+print(AlbumTitle)
+print(Genre)
+print(DurationInSeconds)
+print(SalesCertification)
+print(RecordLabel)
+print(Producer)
+print(CoProducer)
+print(SongWriter)
