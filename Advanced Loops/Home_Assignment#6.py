4d8618a476d6052e67560f0354f8f5e5de32487f
YourFavCoder <chinedumc@users.noreply.github.com>
2018-12-19 22:28:21 +0100

    Create Home_Assignment#6.py

---
+++ Advanced Loops/Home_Assignment#6.py
@@ -0,0 +1,24 @@
+'''
+A program to print out the following pattern shown below: 
+ | | 
+-----
+ | | 
+-----
+ | |  
+'''
+
+for row in range(1,6): # Defining our pattern to have 5 rows
+    if row%2 !=0:    
+        for column in range(5): # Defining our pattern to have 5 columns
+           
+            if column%2 ==0:    # To ensure we get the " " and "|" in the odd rows 
+                if column < 4:  # To ensure we do not get new lines from the print function for all columns in the odd rows:
+                    print(" ", end = "")
+                else:
+                    print(" ")
+                
+            else:
+                print("|", end = "")
+            
+    else:
+        print("-----")        
