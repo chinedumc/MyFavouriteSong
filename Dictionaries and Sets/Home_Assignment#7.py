850e8896c08aa91021cf84e2acf59c695cb425fe
YourFavCoder <chinedumc@users.noreply.github.com>
2018-12-20 16:24:13 +0100

    Create Home_Assignment#7.py

---
+++ Dictionaries and Sets/Home_Assignment#7.py
@@ -0,0 +1,21 @@
+"""
+A program that takes the features of my favourite song into a dictionary 
+And then uses one print statement to print out the property of each feature in a 
+key-value pair.
+"""
+
+# Define the dictionary using features from My Faourite Song
+FavSongDict = {"SongTitle" : "Bad", 
+               "Artist" : "Michael Jackson",
+               "YearReleased":"1987",
+               "AlbumTitle" : "Bad",
+               "Genre" : "Pop",
+               "DurationInSeconds" : "246",
+               "SalesCert" : "Platinum",
+               "RecordLabel" : "Epic",
+               "Producer" : "Quincy Jones",
+               "CoProducer" : "Michael Jackson",
+               "SongWriter" : "Michael Jackson"}
+
+for feature in FavSongDict:
+    print(feature + " = " + FavSongDict[feature]) #Print each feature and it's value
