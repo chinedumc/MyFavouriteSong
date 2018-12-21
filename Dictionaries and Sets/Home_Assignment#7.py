4a78da3bcd8d1235f04dcf906d1f7765038af86c
YourFavCoder <chinedumc@users.noreply.github.com>
2018-12-21 15:09:29 +0100

    Update Home_Assignment#7.py

--- Dictionaries and Sets/Home_Assignment#7.py
+++ Dictionaries and Sets/Home_Assignment#7.py
@@ -19,3 +19,21 @@ FavSongDict = {"SongTitle" : "Bad",
 
 for feature in FavSongDict:
     print(feature + " = " + FavSongDict[feature]) #Print each feature and it's value
+    
+    
+'''
+A function to checkthe Key-Value pair entered for a Feature and its corresponding 
+value in the FavSongDict dictionary of My FavouriteSong.
+When the Key and Value are correct, True is returned. But when any 
+wrong Key or Value is entered, the output False is printed to the screen
+'''
+Enter_FeatureValue = FavSongDict[feature]
+
+def checkFeature(feature, Enter_FeatureValue):
+    if (Enter_FeatureValue) == (FavSongDict[feature]):
+        print(True)
+    else:
+        print(False)   
+    
+checkFeature("SalesCert", "Platinum") #Output is True
+checkFeature("CoProducer", "Chinedum") #Output is False
